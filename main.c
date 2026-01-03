844c3f494afda59340158b093410773b0250f6df
abduznik <85239936+abduznik@users.noreply.github.com>
2026-01-03 19:51:33 +0200

    feat: implement read-only serial monitor

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include <stdio.h>
 
+HANDLE hSerial;
+
 // Helper to list ports on Windows using the Registry
 void list_ports() {
     char path[256];
@@ -37,10 +39,87 @@ void list_ports() {
     }
 }
 
+void configure_port(const char* portName)
+{
+    char fullPortName[32];
+    // Ports above COM9 need a prefix "\\.\"
+    snprintf(fullPortName, sizeof(fullPortName), "\\\\.\\%s", portName);
+
+    hSerial = CreateFile(fullPortName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hSerial == INVALID_HANDLE_VALUE)
+    {
+        printf("Error: Could not open %s (Code: %lu)\n", portName, GetLastError);
+        exit(1);
+    }
+
+    DCB dcbSerialParams = {0};
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+
+    if (!GetCommState(hSerial, &dcbSerialParams))
+    {
+        printf("Error: Could not get state.\n");
+        CloseHandle(hSerial);
+        exit(1);
+    }
+
+    dcbSerialParams.BaudRate = CBR_115200;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity = NOPARITY;
+
+    if (!SetCommState(hSerial, &dcbSerialParams))
+    {
+        printf("Error: Could not set serial params.\n");
+        CloseHandle(hSerial);
+        exit(1);
+    }
+
+    // Set Timeouts
+    COMMTIMEOUTS timeouts = {0};
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    SetCommTimeouts(hSerial, &timeouts);
+
+    printf("Connected to %s at 115200 baud.\n", portName);
+}   
+
+void read_loop()
+{
+    char buffer[128];
+    DWORD bytesRead;
+
+    printf("--- Monitor (Press CTRL+C to Exit) ---\n");
+
+    while(1)
+    {
+        //Readfile will wait based on our timeouts
+        if (ReadFile(hSerial, buffer, sizeof(buffer) - 1, &bytesRead, NULL))
+        {
+            if (bytesRead > 0)
+            {
+                buffer[bytesRead] = '\0';
+                printf("%s", buffer);
+            }
+        }
+    }
+}
+
 int main() {
-    printf("ESP32 Host Tool v1.0 (Windows Native)\n");
+    char portName[16];
+
     list_ports();
-    printf("\nPress Enter to exit...");
-    getchar(); // Keep window open
+
+    printf("\nEnter COM port (e.g, COM3): ");
+    scanf("%s", portName);
+
+    configure_port(portName);
+
+    // While Loop until window closed
+    read_loop();
+
+    CloseHandle(hSerial);
+
     return 0;
 }
